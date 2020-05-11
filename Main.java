0b4c9ab1aaf393988c530b748538e16fd26bc9b4
mikochild2 <mikochild2@gmail.com>
2020-05-11 02:24:11 -0400

    last update 5/11

--- Main.java
+++ Main.java
@@ -6,136 +6,148 @@ public class Main {
         AsciiChars.printNumbers();
         AsciiChars.printUpperCase();
         AsciiChars.printLowerCase();
-        System.out.println("Look human, I can do tricks.\nJoking\n. . .\nOr not\n 0_o\nOk, no more jokes. Let's get started.\nBy the way..");
+        System.out.println("Look who?man, I can do tricks.\nJoking. . .\nOr not\n 0_o\nOk, no more jokes. Let's get started.\nBy the way..");
 
         //User interaction
 
         //Name
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your name: ");
+        System.out.println("What is your name: ");
         String userName = scanner.next();
-        System.out.println("Hello "+ userName);
+        System.out.println("Hello "+ userName+"\n");
 
         //Continue/Exit
-        System.out.print("Continue or End?");
+        int continueTrigger = 0;
+       // String nameContinueLowerCase; //= nameContinue.toLowerCase(); //tutorialspoint.com/java_string_tolowercase.htm
+        do {
+            System.out.println("Would you like to start the Lottery Survey? Yes or No");
         String nameContinue = scanner.next();
-        String lowerCaseContinue = nameContinue.toLowerCase(); //tutorialspoint.com/java_string_tolowercase.htm
 
-        switch(lowerCaseContinue) {
-            case "continue":
-                break;
-            case "end":
-                System.out.print("Thank you for participating! Please return later to complete the survey.");
-                System.exit(0); //geeksforgeeks.org/system-exit-in-java/
-            default:
-                System.out.print("Not sure what that was, but it wasn't 'Continue' so . . .\nGood bye.");
-                System.exit(0);
+            switch(nameContinue) {
+                case "yes":
+                    System.out.println("\nGreat! Let's get started.");
+                    continueTrigger = 1;
+                    break;
+                case "no":
+                    System.out.printf("\nSorry to see you go %s. Please return later to complete the survey!.", userName);
+                    System.exit(0); //geeksforgeeks.org/system-exit-in-java/
+                default:
+                    System.out.print("\nI'm not sure what that was . . .");
+            }
         }
+        while (continueTrigger == 0);
 
-        //Car
-        System.out.println("Do you have a red car? Yes or No");
-        String redCar = scanner.next();
-
-        //Pet Name
-        System.out.println("Are you a pet owner? Yes or No");
-        String petOwner = scanner.next();
-        String petOwnerLowerCase = petOwner.toLowerCase();
-
-        switch(petOwnerLowerCase) {
-            case "yes":
-                System.out.println("What is the name of your favorite pet?");
-                String petName = scanner.next();
-                if(petName.length() < 5) {
-                    System.out.println("Great name!");
-                }else {
-                    System.out.println("Interesting . . .");
-                };
-                System.out.println("How old is " + petName + "?\nDon't be complicated. No decimals or fractions.");
-                int petAge = scanner.nextInt();
-                break;
-            case "no":
-                System.out.println("I'm sorry for your loss.");
-        }
         
+       
+       // String newSurveyLowerCase = "yes";
+       String newSurvey;
+        do{
+            //Pet Name
+        System.out.println("\nWhat is the first name of your favorite pet? If you don't have one make one up.");
+        String petName = scanner.next();
+        if(petName.length() < 5) {
+            System.out.println("\nGreat name!");
+        }else {
+            System.out.println("\nInteresting . . .");
+        }
+
+        //Pet Age
+        System.out.println("\nHow old is " + petName + "?");
+        int petAge = scanner.nextInt();                
 
         //Lucky Number
-        System.out.println("What is your lucky number?");
+        System.out.println("\nWhat is your lucky number?");
         int luckyNumber = scanner.nextInt();
-        System.out.println("Are you sure . . .\nNevermind. Next Question.");
+        System.out.println("\nAre you sure . . .\nNevermind. Next Question.");
 
         //Quarterback
-        System.out.println("Do you have a favorite quarterback? Yes or No");
-        String favQb = scanner.next();
-        String favQbLowerCase = favQb.toLowerCase();
-
-        switch(favQbLowerCase) {
-            case "yes":
-                System.out.println("What is their jersey number?");                
-                break;
-            case "no":
-                System.out.println("Not much into sports, are you? Ok, just enter any 2 digit number.");
-                break;
-            default:
-                System.out.println("Don't understand yes or no, do you who?man . . . Ok, just enter any 2 digit number.");
-        }
-
+        System.out.println("\nDo you have a favorite quarterback? If so, what is their jersey number? If not, any random 2 digit number will do.");
         int jerseyNumber = scanner.nextInt();
-                if(favQbLowerCase.equals("no")){
-
-                } else if(jerseyNumber%2 == 1){
-                    System.out.println("How about those Bears?!\n. . .\nNo?\n. . .\nNever mind . . . stupid who?mans . . .");
+                if(jerseyNumber%2 == 1){
+                    System.out.println("\nHow about those Bears?!\n. . .\nNo?\n. . .\nNever mind . . . stupid who?mans . . .");
                 } else {
-                    System.out.println("You've got to be joking!\n Guess there's no accounting for taste in a who?man . . .");
+                    System.out.println("\nYou've got to be joking!\n Guess there's no accounting for taste when it comes to who?mans . . .");
                 }
 
         //Car year
-        System.out.println("What is the two-digit model year of your car? Hint: If you don't have a car, lie.");
+        System.out.println("\nWhat is the two-digit model year of your car? Hint: If you don't have a car, lie.");
         int carYear = scanner.nextInt();
 
         if(carYear <13) {
-            System.out.println("Time for a new car, isn't it?");
+            System.out.println("\nTime for a new car, isn't it?");
         } else if(carYear < 17) {
-            System.out.println("Not new, but not bad.");
+            System.out.println("\nNot new, but not bad.");
         } else {
-            System.out.println("Nice!");
+            System.out.println("\nNice!");
         }
 
         //Actor
-        System.out.println("Who is your favorite actor or actress?");
+        System.out.println("\nWhat is the first name of your favorite actor or actress?");
         String favActor = scanner.next();
-        System.out.println("Ok. . .");
+        System.out.println("\nOk. . .");
 
         //Random Number
-        System.out.println("Enter a random number between 1 and 50.");
-        Double randomNum = scanner.nextDouble();
+        int randomNum = 1;
+        do {
+            System.out.println("\nEnter a random number between 1 and 50.");
+            randomNum = scanner.nextInt();
+        }
+        while (randomNum > 50);
+
+
+        //Random Math Numbers
+        Random rand = new Random(); //https://www.geeksforgeeks.org/generating-random-numbers-in-java/
+        int rand_int1 = rand.nextInt(75);
+        int rand_int2 = rand.nextInt(65);
+        int rand_int3 = rand.nextInt(65);
+        int rand_int4 = rand.nextInt(65);
 
         //Magic Ball
-        Random rand = new Random(50); //https://mathbits.com/JavaBitsNotebook/LibraryMethods/RandomGeneration.html
-
-        if(jerseyNumber/2 < luckyNumber*Math.random()/2) {
-            if(luckyNumber*rand.nextInt() < 75) {
-                int magicBall = luckyNumber*rand.nextInt();
-            } else {
-                int magicBall = (luckyNumber*rand.nextInt()) - 75;
-
-            }
-        } else {
-            if(jerseyNumber*rand.nextInt() < 75) {
-                int magicBall = jerseyNumber*rand.nextInt();
-            } else {
-                int magicBall = (jerseyNumber*rand.nextInt()) - 75;
-            }
+        int magicBall = luckyNumber * rand_int1;
+        while (magicBall > 75) {
+            magicBall = magicBall - 75;
         }
 
-        System.out.println("Lottery numbers: "+)
- 
+        //Other Random Numbers
+
+        //Pet
+        int petInput = petName.toUpperCase().charAt(2)-25;
+        int petValue = petInput;
+
+        //Car Year
+        int carValue = carYear*luckyNumber;
+        while (carValue > 65) {
+            carValue = carValue - 65;
+        }
+
+        //Favorite Actor
+        int actorInput = favActor.toUpperCase().charAt(0)-25;
+        int actorValue = actorInput;
+
+        //Pet Car
+        int petCarValue = petAge + carYear;
+        while (petCarValue > 65) {
+            petCarValue = petCarValue-65;
+        }
+
+        //QB petAge luckyNumber
+        int mixedValue = jerseyNumber + petAge + luckyNumber;
+        while (mixedValue > 65) {
+            mixedValue = mixedValue - 65;
+        }
+
+        System.out.printf("\nLottery numbers: %s, %S, %s, %s, %s Magic ball: %s", petValue, carValue, actorValue, petCarValue, mixedValue, magicBall);
+        
+        //Again?
+        System.out.println("\nWould you like to reanswer the questions for more lottery numbers? Yes or No?");
+        newSurvey = scanner.next();
+        //newSurveyLowerCase = newSurvey.toLowerCase();
+        }
+        while (newSurvey.equalsIgnoreCase("yes")); // https://www.geeksforgeeks.org/compare-two-strings-in-java/
+        
+        System.out.println("\nHave a great day!");
         
 
-
-
-
-            
-                
 
         scanner.close();
 
